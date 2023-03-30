6962ce129ecd7be29f59d4cb553738725eba5d3e
Seymur Bagirov <megadestroyer230@gmail.com>
2023-03-31 01:33:15 +0400

    add model configuration

--- WebApplication2/Data/HistoryFiguresDbContext.cs
+++ WebApplication2/Data/HistoryFiguresDbContext.cs
@@ -10,7 +10,21 @@ public class HistoryFiguresDbContext : DbContext
     }
 
 
-    public DbSet<HistoricalFigure> HistoricalFigures { get; set; } = null!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<HistoricalFigure>(e =>
+        {
+            e.HasKey(x => x.Id);
+            e.Property(x => x.Id).ValueGeneratedOnAdd().IsRequired();
+            e.Property(x => x.Name).IsRequired();
+            e.Property(x => x.Surname).IsRequired();
+            e.Property(x => x.Occupation).IsRequired();
+            e.Property(x => x.DateOfBirth).IsRequired();
+            e.Property(x => x.DateOfDeath).IsRequired();
+            e.Property(x => x.Description).IsRequired();
+        });
+    }
 
-    
+
+    public DbSet<HistoricalFigure> HistoricalFigures { get; set; } = null!;
 }
